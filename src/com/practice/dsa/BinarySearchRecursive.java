2c8fe3700e9bc464f43cc32eb57b30d2b8e6b2cd
KUMAG032 <gaurav.kumar.-nd3@disney.com>
2026-04-27 20:41:17 +0530

    BinarySearchRecursive Method

---
+++ src/com/practice/dsa/BinarySearchRecursive.java
@@ -0,0 +1,31 @@
+package com.practice.dsa;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class BinarySearchRecursive {
+    static Logger logger = Logger.getLogger(BinarySearchRecursive.class.getName());
+
+    public int binarySearch(int[] arr, int key, int leftIndex, int rightIndex) {
+        if (leftIndex > rightIndex) {
+            return -1;
+        } else {
+            int middleIndex =  (leftIndex + rightIndex) / 2;
+            if (key == arr[middleIndex]) {
+                return middleIndex;
+            } else if (key < arr[middleIndex]) {
+                return binarySearch(arr, key, leftIndex, middleIndex-1);
+            } else {
+                return binarySearch(arr, key, middleIndex+1, rightIndex);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        logger.info("Binary Search Recursive Algorithm");
+        BinarySearchRecursive bs = new BinarySearchRecursive();
+        int result = bs.binarySearch(new int[]{4, 11, 18, 30, 54}, 36, 0, 4);
+        logger.log(Level.INFO, "Search index for 36: {0} (-1 = not found)", result);
+        result = bs.binarySearch(new int[]{4, 11, 18, 30, 54}, 30, 0, 4);
+        logger.log(Level.INFO, "Search index for 30: {0} (-1 = not found)", result);
+    }
+}
